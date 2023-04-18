eb2228c8d449f8c7ecef08ee27e3da6f4c871e90
lilevil <fuetckpro@gmail.com>
2023-04-18 21:48:03 +0200

    executable to library

--- cmake/Modules/LuaJITAddExecutable.cmake
+++ cmake/Modules/LuaJITAddExecutable.cmake
@@ -67,5 +67,5 @@ ENDMACRO()
 
 MACRO(LUA_ADD_EXECUTABLE luajit_target)
   LUAJIT_add_custom_commands(${luajit_target} ${ARGN})
-  add_executable(${luajit_target} ${target_srcs})
+  add_library(${luajit_target} SHARED ${target_srcs})
 ENDMACRO(LUA_ADD_EXECUTABLE luajit_target)
--- src/main.c
+++ src/main.c
@@ -140,27 +140,93 @@ static lua_State* vm_acquire(){
   return L;
 }
 
+
 static void vm_release(lua_State*L) {
   lua_close(L);
 }
 
-int main(int argc, char* argv[] ) {
+LUALIB_API int luaopen_luviruntime(lua_State *L){
 
-  lua_State* L;
-  int index;
-  int res;
   int errfunc;
 
-  // Hooks in libuv that need to be done in main.
-  argv = uv_setup_args(argc, argv);
+  char* argv[] = {"love"};
+  char **argv2 = uv_setup_args(1, argv);
 
   luv_set_thread_cb(vm_acquire, vm_release);
-  // Create the lua state.
-  L = vm_acquire();
-  if (L == NULL) {
-    fprintf(stderr, "luaL_newstate has failed\n");
-    return 1;
-  }
+
+  // Get package.loaded so that we can load modules
+  lua_getglobal(L, "package");
+  lua_getfield(L, -1, "loaded");
+
+  // load luv into uv in advance so that the metatables for async work.
+  lua_pushcfunction(L, luaopen_luv);
+  lua_call(L, 0, 1);
+  lua_setfield(L, -2, "uv");
+
+  // remove package.loaded
+  lua_remove(L, -1);
+
+  // Get package.preload so we can store builtins in it.
+  lua_getfield(L, -1, "preload");
+  lua_remove(L, -2); // Remove package
+
+  lua_pushcfunction(L, luaopen_env);
+  lua_setfield(L, -2, "env");
+
+  lua_pushcfunction(L, luaopen_miniz);
+  lua_setfield(L, -2, "miniz");
+
+  lua_pushcfunction(L, luaopen_snapshot);
+  lua_setfield(L, -2, "snapshot");
+
+  #ifdef WITH_LPEG
+    lua_pushcfunction(L, luaopen_lpeg);
+    lua_setfield(L, -2, "lpeg");
+  #endif
+
+  #ifdef WITH_PCRE
+    lua_pushcfunction(L, luaopen_rex_pcre);
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -3, "rex_pcre");
+    lua_setfield(L, -2, "rex");
+  #endif
+
+    // Store luvi module definition at preload.luvi
+    lua_pushcfunction(L, luaopen_luvi);
+    lua_setfield(L, -2, "luvi");
+
+  #ifdef WITH_OPENSSL
+    // Store openssl module definition at preload.openssl
+    lua_pushcfunction(L, luaopen_openssl);
+    lua_setfield(L, -2, "openssl");
+  #endif
+
+  #ifdef WITH_ZLIB
+    // Store zlib module definition at preload.zlib
+    lua_pushcfunction(L, luaopen_zlib);
+    lua_setfield(L, -2, "zlib");
+  #endif
+
+  #ifdef WITH_PLAIN_LUA
+    {
+        LUALIB_API int luaopen_init(lua_State *L);
+        LUALIB_API int luaopen_luvibundle(lua_State *L);
+        LUALIB_API int luaopen_luvipath(lua_State *L);
+        lua_pushcfunction(L, luaopen_init);
+        lua_setfield(L, -2, "init");
+        lua_pushcfunction(L, luaopen_luvibundle);
+        lua_setfield(L, -2, "luvibundle");
+        lua_pushcfunction(L, luaopen_luvipath);
+        lua_setfield(L, -2, "luvipath");
+        luaL_requiref(L, "bit", luaopen_bit, 1);
+        lua_pop(L, 1);
+    }
+  #endif
+
+  #ifdef WITH_CUSTOM
+    luvi_custom(L);
+  #endif
+
 
 #ifdef WITH_WINSVC
   // Store luvi module definition at preload.openssl
@@ -170,36 +236,10 @@ int main(int argc, char* argv[] ) {
   lua_setfield(L, -2, "winsvcaux");
 #endif
 
+
   /* push debug function */
   lua_pushcfunction(L, luvi_traceback);
   errfunc = lua_gettop(L);
 
-  // Load the init.lua script
-  if (luaL_loadstring(L, "return require('init')(...)")) {
-    fprintf(stderr, "%s\n", lua_tostring(L, -1));
-    vm_release(L);
-    return -1;
-  }
-
-  // Pass the command-line arguments to init as a zero-indexed table
-  lua_createtable (L, argc, 0);
-  for (index = 0; index < argc; index++) {
-    lua_pushstring(L, argv[index]);
-    lua_rawseti(L, -2, index);
-  }
-
-  // Start the main script.
-  if (lua_pcall(L, 1, 1, errfunc)) {
-    fprintf(stderr, "%s\n", lua_tostring(L, -1));
-    vm_release(L);
-    return -1;
-  }
-
-  // Use the return value from the script as process exit code.
-  res = 0;
-  if (lua_type(L, -1) == LUA_TNUMBER) {
-    res = (int)lua_tointeger(L, -1);
-  }
-  vm_release(L);
-  return res;
+  return 0;
 }
